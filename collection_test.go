a97a29d784cdc14066fc362ccd721fa76bad2868
Kimmo Lehto <klehto@mirantis.com>
2023-09-05 10:30:13 +0300

    Add JSON/YAML marshalling/unmarshalling (#5)
    * Add JSON/YAML marshalling/unmarshalling
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- collection_test.go
+++ collection_test.go
@@ -1,6 +1,7 @@
 package version
 
 import (
+	"encoding/json"
 	"sort"
 	"testing"
 
@@ -32,4 +33,62 @@ func TestSorting(t *testing.T) {
 	assert.Equal(t, "v1.21.1+k0s.2", c[2].String())
 	assert.Equal(t, "v1.21.2-beta.1+k0s.0", c[3].String())
 	assert.Equal(t, "v1.21.2+k0s.0", c[4].String())
+}
+
+func TestCollectionMarshalling(t *testing.T) {
+	c, err := NewCollection("v1.0.0+k0s.0", "v1.0.1+k0s.0")
+	assert.NoError(t, err)
+
+	t.Run("JSON", func(t *testing.T) {
+		jsonData, err := json.Marshal(c)
+		assert.NoError(t, err)
+		assert.Equal(t, `["v1.0.0+k0s.0","v1.0.1+k0s.0"]`, string(jsonData))
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		yamlData, err := c.MarshalYAML()
+		assert.NoError(t, err)
+		assert.Equal(t, []string{`"v1.0.0+k0s.0"`, `"v1.0.1+k0s.0"`}, yamlData)
+	})
+}
+
+func TestCollectionUnmarshalling(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {
+		var c Collection
+		err := json.Unmarshal([]byte(`["v1.0.0+k0s.1","v1.0.1+k0s.1"]`), &c)
+		assert.NoError(t, err)
+		assert.Equal(t, "v1.0.0+k0s.1", c[0].String())
+		assert.Equal(t, "v1.0.1+k0s.1", c[1].String())
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		var c Collection
+
+		err := c.UnmarshalYAML(func(i interface{}) error {
+			*(i.(*[]string)) = []string{"v1.0.0+k0s.1", "v1.0.1+k0s.1"}
+			return nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, "v1.0.0+k0s.1", c[0].String())
+		assert.Equal(t, "v1.0.1+k0s.1", c[1].String())
+	})
+}
+
+func TestFailingCollectionUnmarshalling(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {
+		var c Collection
+		err := json.Unmarshal([]byte(`invalid_json`), &c)
+		assert.Error(t, err)
+		err = json.Unmarshal([]byte(`["invalid_version"]`), &c)
+		assert.Error(t, err)
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		var c Collection
+		err := c.UnmarshalYAML(func(i interface{}) error {
+			*(i.(*[]string)) = []string{"invalid\n"}
+			return nil
+		})
+		assert.Error(t, err)
+	})
 }
--- version_test.go
+++ version_test.go
@@ -1,6 +1,8 @@
 package version
 
 import (
+	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -43,4 +45,63 @@ func TestURLs(t *testing.T) {
 	assert.Equal(t, "https://github.com/k0sproject/k0s/releases/download/v1.23.3%2Bk0s.1/k0s-v1.23.3+k0s.1-amd64.exe", a.DownloadURL("windows", "amd64"))
 	assert.Equal(t, "https://github.com/k0sproject/k0s/releases/download/v1.23.3%2Bk0s.1/k0s-v1.23.3+k0s.1-arm64", a.DownloadURL("linux", "arm64"))
 	assert.Equal(t, "https://docs.k0sproject.io/v1.23.3+k0s.1/", a.DocsURL())
+}
+
+func TestMarshalling(t *testing.T) {
+	v, err := NewVersion("v1.0.0+k0s.0")
+	assert.NoError(t, err)
+
+	t.Run("JSON", func(t *testing.T) {
+		jsonData, err := json.Marshal(v)
+		assert.NoError(t, err)
+		assert.Equal(t, `"v1.0.0+k0s.0"`, string(jsonData))
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		yamlData, err := v.MarshalYAML()
+		assert.NoError(t, err)
+		assert.Equal(t, "v1.0.0+k0s.0", yamlData)
+	})
+}
+
+func TestUnmarshalling(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {
+		var v Version
+		err := json.Unmarshal([]byte(`"v1.0.0+k0s.1"`), &v)
+		assert.NoError(t, err)
+		assert.Equal(t, "v1.0.0+k0s.1", v.String())
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		v := &Version{}
+		err := v.UnmarshalYAML(func(i interface{}) error {
+			*(i.(*string)) = "v1.0.0+k0s.1"
+			return nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, "v1.0.0+k0s.1", v.String())
+	})
+}
+
+func TestFailingUnmarshalling(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {
+		var v Version
+		err := json.Unmarshal([]byte(`invalid_json`), &v)
+		assert.Error(t, err)
+		err = json.Unmarshal([]byte(`"invalid_version"`), &v)
+		assert.Error(t, err)
+	})
+
+	t.Run("YAML", func(t *testing.T) {
+		var v = &Version{}
+		err := v.UnmarshalYAML(func(i interface{}) error {
+			return errors.New("forced error")
+		})
+		assert.Error(t, err)
+		err = v.UnmarshalYAML(func(i interface{}) error {
+			*(i.(*string)) = "invalid_version"
+			return nil
+		})
+		assert.Error(t, err)
+	})
 }
