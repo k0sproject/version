9f489b03b3638e6f165597dc4fc299c5a5f9656f
Kimmo Lehto <klehto@mirantis.com>
2024-01-18 13:23:26 +0200

    Add v.Base(), v.Clone() and v.WithK0s(int) (#16)
    * Add v.Clone() and v.WithK0s(int)
    
      v.Clone() returns a copy
    
      v.WithK0s(n int) returns a copy with +k0s.n
    
      Also changed v.K0sVersion() to return -1 instead of 0 when v.IsK0s()
      is false.
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Rename to K0s() and change signature
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Fix assertions
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Comment tweak
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Zero is better than -1
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    ---------
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- version.go
+++ version.go
@@ -135,9 +135,27 @@ func (v *Version) IsK0s() bool {
 	return v.isK0s
 }
 
-// K0sVersion returns the k0s version (eg 4 from v1.2.3-k0s.4)
-func (v *Version) K0sVersion() int {
-	return v.k0s
+// K0s returns the k0s version (eg 4 from v1.2.3-k0s.4) and true if the version is a k0s version. Otherwise it returns 0 and false.
+func (v *Version) K0s() (int, bool) {
+	return v.k0s, v.isK0s
+}
+
+// Base returns the version as a string without the k0s or metadata part (eg v1.2.3+k0s.4 -> v1.2.3)
+func (v *Version) Base() string {
+	return strings.SplitN(v.String(), "+", 2)[0]
+}
+
+// Clone returns a copy of the k0s version
+func (v *Version) Clone() *Version {
+	return &Version{comparableFields: v.comparableFields}
+}
+
+// WithK0s returns a copy of the k0s version with the k0s part set to the supplied value
+func (v *Version) WithK0s(n int) *Version {
+	newV := v.Clone()
+	newV.isK0s = true
+	newV.k0s = n
+	return newV
 }
 
 // Metadata returns the metadata part of the k0s version (eg 123abc from v1.2.3+k0s.1.123abc)
